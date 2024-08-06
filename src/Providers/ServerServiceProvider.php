6639e1d5d4f766fc3eafae1d123b071acd62b659
wpjscc <wpjscc@gmail.com>
2024-08-06 11:40:44 +0800

    first

---
+++ src/Providers/ServerServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+namespace Reactphp\Framework\LaravelLog\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Monolog\Logger as Monolog;
+use Monolog\Processor\PsrLogMessageProcessor;
+use React\Filesystem\Factory;
+use Reactphp\Framework\LaravelLog\Handler\StreamHandler;
+use Reactphp\Framework\LaravelLog\Handler\RotatingFileHandler;
+
+class ServerServiceProvider extends ServiceProvider
+{
+    protected $defer = false;
+
+    public function register()
+    {
+        $this->app->singleton("reactphp.filesystem", function ($app) {
+            return Factory::create();
+        });
+    }
+
+    public function boot()
+    {
+        $this->app['log']->extend('single', function($app, $config){
+            return new Monolog($this->parseChannel($config), [
+                $this->prepareHandler(
+                    new StreamHandler(
+                        $config['path'], $this->level($config),
+                        $config['bubble'] ?? true, $config['permission'] ?? null, $config['locking'] ?? false
+                    ), $config
+                ),
+            ], $config['replace_placeholders'] ?? false ? [new PsrLogMessageProcessor()] : []);
+        });
+        $this->app['log']->extend('daily', function($app, $config){
+            return new Monolog($this->parseChannel($config), [
+                $this->prepareHandler(new RotatingFileHandler(
+                    $config['path'], $config['days'] ?? 7, $this->level($config),
+                    $config['bubble'] ?? true, $config['permission'] ?? null, $config['locking'] ?? false
+                ), $config),
+            ], $config['replace_placeholders'] ?? false ? [new PsrLogMessageProcessor()] : []);
+        });
+    }
+
+}
